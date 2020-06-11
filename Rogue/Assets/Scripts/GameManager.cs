00392ef88b59b9035bb80f81970a5401280b3a20
Ryan <rkankarberg@gmail.com>
2020-06-11 16:30:21 -0600

    update
    update

--- Rogue/Assets/Scripts/GameManager.cs
+++ Rogue/Assets/Scripts/GameManager.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+    private int level = 3;
+
+
+    void Awake()
     {
-        
+        // make sure we only have one instance of game running
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        // to be sure things persist between scenes, we do this
+        DontDestroyOnLoad(gameObject);
+
+        // get and store a component reference to our BoardManager script
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        // call the SetupScene function of boardScript
+        boardScript.SetupScene(level);
+
     }
 
     // Update is called once per frame
---
+++ Rogue/Assets/Scripts/Loader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Purpose of this script is to load the GameManager when the game starts.  Checks if GameManager has been instantiated,
+/// and if not, will do so from our prefab
+/// </summary>
+/// 
+public class Loader : MonoBehaviour
+{
+    public GameObject gameManager;
+    void Awake()
+    {
+        // check if game manager instance is null
+        if(GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
+    }
+}
